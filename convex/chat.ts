1633b41135dad111379efc87bcd26d0e2aa88bb5
tavalentine00 <tavalentinecs@gmail.com>
2026-04-08 15:52:00 -0700

    implemented wiki-topic-search

--- convex/chat.ts
+++ convex/chat.ts
@@ -1,5 +1,7 @@
-import { mutation, query } from "./_generated/server";
-import { v } from "convex/values";
+import { internalAction, mutation, query } from "./_generated/server";
+import { v } from "convex/values";  
+import { internal } from "./_generated/api";
+import { api } from "./_generated/api";
 
 export const sendMessage = mutation({
     args: {
@@ -11,6 +13,11 @@ export const sendMessage = mutation({
             user: args.user,
             body: args.body,
         });
+
+        if (args.body.startsWith("/wiki")) {
+            const topic = args.body.slice(args.body.indexOf(" ") + 1);
+            await ctx.scheduler.runAfter(0, internal.chat.getWikipediaSummary, { topic });
+        }
     }
 });
 
@@ -21,3 +28,23 @@ export const getMessages = query({
         return messages.reverse();
     },
 });
+
+export const getWikipediaSummary = internalAction({
+    args: {
+        topic: v.string(),
+    },
+    handler: async (ctx, args) => {
+        const response = await fetch(`https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&titles=${args.topic}`);
+        const summary = getSummaryFromJSON(await response.json());
+
+        await ctx.scheduler.runAfter(0, api.chat.sendMessage, {
+            user: "Wikipedia",
+            body: summary,
+        });
+    },
+});
+
+function getSummaryFromJSON(data: any) {
+    const firstPageId = Object.keys(data.query.pages)[0];
+    return data.query.pages[firstPageId].extract;
+}
